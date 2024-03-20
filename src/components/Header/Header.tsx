fa384088b2ff6ef36a0cdff4ff1a25c4c86b8deb
andrianazaliznak <andrianazaliznyak@gmail.com>
2024-03-21 01:41:34 +0200

    updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import css from './Header.module.css'
 
 const Header = () => {
     return (
         <div>
+            <div className={`${css.Header} ${css[theme]}`}>
+                <div className={css.MovieIcon}>
+                    {/*<h2 >MoviePulse</h2>*/}
+                    <NavLink className={css.Home} to={'/movies'}>MoviePulse</NavLink>
+
+                    <div className={css.icon}></div>
+                </div>
+
+                <div className={css.Links}>
+                    <NavLink className={css.Nav1} to={'/genres'}>Genres</NavLink>
+                    <NavLink className={css.Nav2} to={'/search'}>Search</NavLink>
+                </div>
+
+
+
+
+                <button className={`${css.toggle} ${css[theme]}`} onClick={toggleTheme}></button>
+                <div className={css.UserIcon}>
+                    <div className={css.icon2}></div>
+
+                </div>
 
         </div>
     );
