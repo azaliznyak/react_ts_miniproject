912e88746d56fe3d5fff5c5ee9534a40fc52e838
andrianazaliznak <andrianazaliznyak@gmail.com>
2024-03-21 01:40:07 +0200

    updated

---
+++ src/hoc/ThemeContext.tsx
@@ -0,0 +1,34 @@
+import React, {createContext, useState, useContext, FC, ReactNode} from 'react';
+
+interface ThemeContextType {
+    isDarkMode: boolean;
+    toggleTheme: () => void;
+}
+interface ThemeProviderProps {
+    children: ReactNode;
+}
+
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useTheme = (): ThemeContextType => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+    return context;
+};
+
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleTheme = () => {
+        setIsDarkMode(prevMode => !prevMode);
+    };
+
+    return (
+        <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+    {children}
+    </ThemeContext.Provider>
+);
+};
